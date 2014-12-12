9fa1be62b4afec3543f8b1d35f393657df7a186a
Sunny <sunny.200745@gmail.com>
2014-12-12 15:37:28 +0530

    Id/Class ways for handling of click on game

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+/*// With help of id
 document.getElementById("0").addEventListener("click", function(){
     document.getElementById("0").innerHTML = "X";
 });
@@ -25,4 +25,25 @@ document.getElementById("7").addEventListener("click", function(){
 });
 document.getElementById("8").addEventListener("click", function(){
     document.getElementById("8").innerHTML = "X";
-});
+});*/
+
+
+/*//with help of clickhandler from html
+var clickHandler = function(pos){
+  document.getElementsByClassName("cube")[pos].innerHTML = '0';
+}*/
+
+
+
+//  Using class with combination of id without click handler
+var cubeClass = document.getElementsByClassName("cube");
+
+var clickHandler = function() {
+    var attr = this.getAttribute("id");
+        
+    document.getElementById(attr).innerHTML = "X";
+};
+
+for(var i=0;i<cubeClass.length;i++){
+    cubeClass[i].addEventListener('click', clickHandler, false);
+}
