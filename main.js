f724f60c7672c0a681fea2e7e42a93bd0c1f0f3c
Sunny <sunny.200745@gmail.com>
2014-12-12 15:12:02 +0530

    Added click event in js and on click added innnerhtml

---
+++ main.js
@@ -0,0 +1,28 @@
+
+document.getElementById("0").addEventListener("click", function(){
+    document.getElementById("0").innerHTML = "X";
+});
+document.getElementById("1").addEventListener("click", function(){
+    document.getElementById("1").innerHTML = "X";
+});
+document.getElementById("2").addEventListener("click", function(){
+    document.getElementById("2").innerHTML = "X";
+});
+document.getElementById("3").addEventListener("click", function(){
+    document.getElementById("3").innerHTML = "X";
+});
+document.getElementById("4").addEventListener("click", function(){
+    document.getElementById("4").innerHTML = "X";
+});
+document.getElementById("5").addEventListener("click", function(){
+    document.getElementById("5").innerHTML = "X";
+});
+document.getElementById("6").addEventListener("click", function(){
+    document.getElementById("6").innerHTML = "X";
+});
+document.getElementById("7").addEventListener("click", function(){
+    document.getElementById("7").innerHTML = "X";
+});
+document.getElementById("8").addEventListener("click", function(){
+    document.getElementById("8").innerHTML = "X";
+});
